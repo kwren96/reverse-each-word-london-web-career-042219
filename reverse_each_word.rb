18d446ff50fc0b52c9981044a529e3c7b79cabd7
Katherine Wren <kwren@outlook.com>
2019-04-19 17:46:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,8 @@
 def reverse_each_word(string)
   array = string.split(' ')
-  array2 = []
   array.collect do |word|
     backwards = word.reverse
-    array2 << backwards
   end
-  newstring = array2.join(' ')
+  newstring = array.join(' ')
   return newstring
 end
