1f0ca00a78a2bf640a0c0292dbcf27f3f4719e0e
Katherine Wren <kwren@outlook.com>
2019-04-19 17:47:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   array = string.split(' ')
   array.collect do |word|
-    backwards = word.reverse
+    word.reverse
   end
   newstring = array.join(' ')
   return newstring
